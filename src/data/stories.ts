dd0ec98854ebcb36404ff9c8eb408ea38ba9c11c
Amar Narute <amarnarute@gmail.com>
2025-05-05 02:49:40 +0530

    add more stories

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -21,7 +21,7 @@ export const users: User[] = [
       },
       {
         id: "1-2",
-        imageUrl: getImageUrl(1015), 
+        imageUrl: getImageUrl(1015),
         timestamp: Date.now() - 3500000,
       },
     ],
@@ -33,17 +33,17 @@ export const users: User[] = [
     stories: [
       {
         id: "2-1",
-        imageUrl: getImageUrl(1025), 
+        imageUrl: getImageUrl(1025),
         timestamp: Date.now() - 3400000,
       },
       {
         id: "2-2",
-        imageUrl: getImageUrl(1035), 
+        imageUrl: getImageUrl(1035),
         timestamp: Date.now() - 3300000,
       },
       {
         id: "2-3",
-        imageUrl: getImageUrl(1045), 
+        imageUrl: getImageUrl(1045),
         timestamp: Date.now() - 3200000,
       },
     ],
@@ -57,6 +57,94 @@ export const users: User[] = [
         id: "3-1",
         imageUrl: getImageUrl(1055),
         timestamp: Date.now() - 3100000,
+      },
+    ],
+  },
+  {
+    id: "4",
+    username: "Mitchelle",
+    avatarUrl: getImageUrl(1004, 200, 200),
+    stories: [
+      {
+        id: "4-1",
+        imageUrl: getImageUrl(1065),
+        timestamp: Date.now() - 3400000,
+      },
+      {
+        id: "4-2",
+        imageUrl: getImageUrl(1075),
+        timestamp: Date.now() - 3300000,
+      },
+      {
+        id: "4-3",
+        imageUrl: getImageUrl(85),
+        timestamp: Date.now() - 3200000,
+      },
+    ],
+  },
+  {
+    id: "5",
+    username: "Awinash Narute",
+    avatarUrl: getImageUrl(1005, 200, 200),
+    stories: [
+      {
+        id: "5-1",
+        imageUrl: getImageUrl(95),
+        timestamp: Date.now() - 3400000,
+      },
+      {
+        id: "5-2",
+        imageUrl: getImageUrl(100),
+        timestamp: Date.now() - 3300000,
+      },
+      {
+        id: "5-3",
+        imageUrl: getImageUrl(110),
+        timestamp: Date.now() - 3200000,
+      },
+    ],
+  },
+  {
+    id: "6",
+    username: "Jane",
+    avatarUrl: getImageUrl(1006, 200, 200),
+    stories: [
+      {
+        id: "6-1",
+        imageUrl: getImageUrl(120),
+        timestamp: Date.now() - 3400000,
+      },
+      {
+        id: "6-2",
+        imageUrl: getImageUrl(130),
+        timestamp: Date.now() - 3300000,
+      },
+      {
+        id: "6-3",
+        imageUrl: getImageUrl(140),
+        timestamp: Date.now() - 3200000,
+      },
+    ],
+  },
+  {
+    id: "7",
+    username: "Adam",
+    avatarUrl: getImageUrl(142, 200, 200),
+    stories: [
+      {
+        id: "7-1",
+        imageUrl: getImageUrl(145),
+        timestamp: Date.now() - 3400000,
+      },
+      {
+        id: "7-2",
+        imageUrl: getImageUrl(160),
+        timestamp: Date.now() - 3300000,
+      },
+      {
+        id: "7-3",
+        imageUrl: getImageUrl(170),
+        timestamp: Date.now() - 3200000,
       },
     ],
   },
